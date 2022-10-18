48b60ae564e3c6ef5d84435e878101386287a720
Raffaele Intorcia <raffaele.intorcia@gmail.com>
2022-10-18 22:02:16 +0200

    Converting to C128 game

--- main.asm
+++ main.asm
@@ -1,23 +1,27 @@
 // Retro racing game jam
 // Raffaele Intorcia
 
-.segmentdef Code [start=$0810]
+.segmentdef Code [start=$1c10]
 
-.file [name="./main.prg", segments="Code", modify="BasicUpstart", _start=$0810]
+.file [name="./main.prg", segments="Code"]
 .disk [filename="./main.d64", name="MAIN", id="C2022", showInfo]
 {
   [name="--- RAFFAELE ---", type="usr"],
   [name="--- INTORCIA ---", type="usr"],
   [name="-- @GMAIL.COM --", type="usr"],
   [name="----------------", type="usr"],
-  [name="MAIN", type="prg", segments="Code", modify="BasicUpstart", _start=$0810],
+  [name="MAIN", type="prg", segments="Code"],
 }
 
 .segment Code
 
-* = $0810 "Entry"
+c128lib_BasicUpstart128($1c10)
+* = $1c10 "Entry"
 Entry: {
     inc $d020
 
     rts
 }
+
+#import "./common/lib/common.asm"
+#import "./common/lib/common-global.asm"
